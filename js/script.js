03473843c0cf9bcab071528683d738c1397a5573
Wellington <wellingtonalexandre857@gmail.com>
2025-01-29 18:32:47 -0300

    feat: configuração basica do css e js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const navbar = document.querySelector('.navbar');
+
+navbar.innerHTML = `
+<a href="index.html" class="link">
+    Home
+</a>
+<a href="sobre.html" class="link">
+    Sobre
+</a>
+<a href="local.html" class="link">
+    Local
+</a>
+`
